d367605e884fe76f8af119765d6f5387d83da23f
isabellaly <yi.isabella@aol.com>
2014-04-14 12:57:55 -0400

    Try commit

--- MajorityWin/src/com/cmu/action/GetRoomInfo.java
+++ MajorityWin/src/com/cmu/action/GetRoomInfo.java
@@ -1,14 +1,5 @@
 package com.cmu.action;
 
-import java.io.IOException;
-import java.util.ArrayList;
-
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import com.cmu.service.RoomService;
 
 /**
@@ -17,40 +8,45 @@ import com.cmu.service.RoomService;
 @WebServlet("/GetRoomInfo")
 public class GetRoomInfo extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public GetRoomInfo() {
-        super();
-    }
+	private static final long servialUID = 2L;
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public GetRoomInfo() {
+		super();
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		String roomID = request.getParameter("roomID");
 		int id = Integer.parseInt(roomID.trim());
-		if(!RoomService.isRoomExist(id)){
+		if (!RoomService.isRoomExist(id)) {
 			response.getOutputStream().write(new String("NotExist").getBytes());
 			return;
 		}
 		ArrayList<String> people = RoomService.getPeopleInRoom(id);
 		String result = "";
-		if(people.size() == 1){
+		if (people.size() == 1) {
 			result = people.get(0);
-		}else if(people.size() > 1){
-			for(String p: people){
-				result = result + p +",";
+		} else if (people.size() > 1) {
+			for (String p : people) {
+				result = result + p + ",";
 			}
 		}
 		response.getOutputStream().write(result.getBytes());
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		this.doGet(request, response);
 	}
 
